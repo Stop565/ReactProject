c42f5911f54360589a1e8f21f7c99d6e0364f4df
Volodymyr <vova2000sk@gmail.com>
2023-03-24 13:53:56 +0200

    commit

--- app/src/Home/Home.js
+++ app/src/Home/Home.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import Card from '../Card/Card';
 import './home.css'
-
+import { useState } from "react";
 
 
 const Home = ({ info, category }) => {
+
+    const [valCat, setValcat] = useState(false);
+
     return (
         <div className='containerHome'>
             <div className='categorys'>
                 {
-                    category.map((el) => <p className='category' key={el.id}>{el.name}</p>)
+                    category.map((el) => {
+                        return (
+                            <div className='category' key={el.id} onClick={() => setValcat(el.name)} >{el.name}</div>
+                        )
+                    })
                 }
             </div>
             <div className='cards'>
                 {
-                    info.map((el) => <Card key={el.id} el={el} />)
+                    info.map((el) => {
+                        if (valCat === el.category.name) return <Card key={el.id} el={el} />
+                        else if (valCat === false) return <Card key={el.id} el={el} />
+                    })
                 }
             </div>
         </div>
